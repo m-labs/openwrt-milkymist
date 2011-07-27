d3e2e1691825429effd02c9151ded0ab4c655fb7
Lars-Peter Clausen <lars@metafoo.de>
2011-07-27 19:59:19 +0200

    Revert "Clone git repositories with '--depth 1'"
    This reverts commit 0fbc1903f8d516e03af319419ae90ea1811dbe55.
    
    It doesn't work with git over gttp

--- include/download.mk
+++ include/download.mk
@@ -87,7 +87,7 @@ define DownloadMethod/git
 		cd $(TMP_DIR)/dl && \
 		rm -rf $(SUBDIR) && \
 		[ \! -d $(SUBDIR) ] && \
-		git clone --depth 1 $(URL) $(SUBDIR) && \
+		git clone $(URL) $(SUBDIR) && \
 		(cd $(SUBDIR) && git checkout $(VERSION)) && \
 		echo "Packing checkout..." && \
 		rm -rf $(SUBDIR)/.git && \
